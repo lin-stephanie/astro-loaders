786280c3951f1fcdaad5a43b05215f9964b07fcd
Stephanie Lin <102728558+lin-stephanie@users.noreply.github.com>
2024-11-17 23:49:42 +0800

    test(releases): add `userCommit.test.ts`

---
+++ packages/astro-loader-github-releases/test/data/userCommit/releasesData.ts
@@ -0,0 +1,36 @@
+const releasesData = [
+  {
+    id: '43710920442',
+    repoName: 'lin-stephanie/rehype-callouts',
+    repoUrl: 'https://github.com/lin-stephanie/rehype-callouts',
+    releaseVersion: '1.3.0',
+    releaseUrl:
+      'https://github.com/lin-stephanie/rehype-callouts/releases/tag/1.3.0',
+    commitMessage: 'chore: release 1.3.0',
+    commitSha: '472bbddefc02fcb47ab996f9a0ea19de856706b5',
+    commitUrl:
+      'https://github.com/lin-stephanie/rehype-callouts/commit/472bbddefc02fcb47ab996f9a0ea19de856706b5',
+    actorLogin: 'lin-stephanie',
+    actorAvatarUrl: 'https://avatars.githubusercontent.com/u/102728558?',
+    isOrg: false,
+    createdAt: '2024-11-11T17:16:41Z',
+  },
+  {
+    id: '43710920442',
+    repoName: 'lin-stephanie/rehype-callouts',
+    repoUrl: 'https://github.com/lin-stephanie/rehype-callouts',
+    releaseVersion: 'v1.3.0',
+    releaseUrl:
+      'https://github.com/lin-stephanie/rehype-callouts/releases/tag/v1.3.0',
+    commitMessage: 'chore: release 1.3.0',
+    commitSha: '472bbddefc02fcb47ab996f9a0ea19de856706b5',
+    commitUrl:
+      'https://github.com/lin-stephanie/rehype-callouts/commit/472bbddefc02fcb47ab996f9a0ea19de856706b5',
+    actorLogin: 'lin-stephanie',
+    actorAvatarUrl: 'https://avatars.githubusercontent.com/u/102728558?',
+    isOrg: false,
+    createdAt: '2024-11-11T17:16:41Z',
+  },
+]
+
+export default releasesData
